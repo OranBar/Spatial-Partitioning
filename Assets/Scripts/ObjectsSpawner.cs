4dafdfef1ea5b4691d179143d98185b6badd027d
Orabi Barkenobi <oranbar@gmail.com>
2024-01-26 14:35:25 +0100

    .

--- Assets/Scripts/ObjectsSpawner.cs
+++ Assets/Scripts/ObjectsSpawner.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using NaughtyAttributes;
+using BarbarO.ExtensionMethods;
 
 public class ObjectsSpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int objs_to_spawn;
+    public GameObject obj_prefab;
+
+    [Auto]
+    private BoxCollider2D spawnArea;
+
+    [Button]
+    void SpawnObjects()
     {
-        
+        for (int i = 0; i < objs_to_spawn; i++)
+        {
+            Vector3 new_point = spawnArea.bounds.RandomPointInBounds();
+            GameObject new_go = Instantiate(obj_prefab, new_point, Quaternion.identity, this.transform);
+        }
+    }
+    
+    void Start(){
+        SpawnObjects();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
