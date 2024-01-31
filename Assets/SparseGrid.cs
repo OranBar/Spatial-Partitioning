8ba6c10964ea427a7cd4c5764608fc0d0ec11784
Orabi Barkenobi <oranbar@gmail.com>
2024-02-01 00:59:04 +0100

    I think the sparse grid works

--- Assets/SparseGrid.cs
+++ Assets/SparseGrid.cs
@@ -1,17 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEditor.Tilemaps;
 using UnityEngine;
 
-public class SparseGrid<T>
+public class SparseGrid
 {
     public class SparseGridCell {
-        public List<T> elements;
+        public List<GameObject> elements;
         public Bounds bounds;
 
         public SparseGridCell(Vector3 center)
         {
-            elements = new List<T>();
+            elements = new List<GameObject>();
             bounds = new Bounds(
                 center, 
                 Vector3.one * grid_cell_size
@@ -22,7 +23,8 @@ public class SparseGrid<T>
 
 
     public Dictionary<Vector3, SparseGridCell> grid;
-    private const int grid_cell_size = 1; 
+    private const int grid_cell_size = 10; 
+    // private const int grid_cell_size = 1; 
     // private const int grid_cell_size = 10000; 
 
     public SparseGrid()
@@ -30,8 +32,8 @@ public class SparseGrid<T>
         this.grid = new Dictionary<Vector3, SparseGridCell>();
     }
 
-    public void Add(T obj_to_add, Vector3 pos){
-        Vector3 obj_grid_cell = GetObj_GridCell(pos);
+    public void Add(GameObject obj_to_add, Vector3 pos){
+        Vector3 obj_grid_cell = GetCellKey_ForPosition(pos);
 
         if(grid.ContainsKey(obj_grid_cell) == false){
             grid[obj_grid_cell] = new SparseGridCell(pos);
@@ -40,22 +42,54 @@ public class SparseGrid<T>
         grid[obj_grid_cell].elements.Add(obj_to_add);
     }
     
-    private Vector3 GetObj_GridCell(Vector3 pos){
+    private Vector3 GetCellKey_ForPosition(Vector3 pos){
         return new Vector3(
             (int)(pos.x / grid_cell_size),
             (int)(pos.y / grid_cell_size),
             (int)(pos.z / grid_cell_size)
         );
     }
+
+
+    private SparseGridCell GetGridCell(Vector3 pos)
+    {
+        Vector3 cell_key = GetCellKey_ForPosition(pos);
+        return grid[cell_key];
+    }
     
-    public void Remove(T obj_to_remove, Vector3 pos){
-        Vector3 obj_grid_cell = GetObj_GridCell(pos);
+    public void Remove(GameObject obj_to_remove, Vector3 pos){
+        Vector3 obj_grid_cell = GetCellKey_ForPosition(pos);
 
         if(grid[obj_grid_cell] == null){
             throw new System.Exception("Object not in grid");
         }
 
         grid[obj_grid_cell].elements.Remove(obj_to_remove);
+    }
+    
+    public List<GameObject> Search(Bounds search_area){
+        List<GameObject> result = new List<GameObject>();
+
+        // List<SparseGridCell> cells_to_check = new List<SparseGridCell>();
+        Vector3 min_cell = GetCellKey_ForPosition(search_area.min);
+        Vector3 max_cell = GetCellKey_ForPosition(search_area.max);
+        for (int x = (int) min_cell.x; x <= (int) max_cell.x; x++){
+            for (int y = (int) min_cell.y; y <= (int) max_cell.y; y++){
+                for (int z = (int) min_cell.z; z <= (int) max_cell.z; z++){
+
+                    Vector3 curr_key = new Vector3(x, y, z); 
+                    if(grid.ContainsKey(curr_key)){
+                        foreach(var c_elem in grid[curr_key].elements){
+                            if(search_area.Contains( c_elem.transform.position )){
+                                result.Add(c_elem);
+                            }
+                        }
+                    }
+
+                }
+            }
+        }
+        return result;
     }
 
     
--- Assets/SparseGridTester.cs
+++ Assets/SparseGridTester.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using NaughtyAttributes;
 using UnityEngine;
 
@@ -7,19 +8,76 @@ public class SparseGridTester : MonoBehaviour
 {
     [Auto]
     private ObjectsSpawner spawner;
-    private SparseGrid<GameObject> sparse_grid;
+    private SparseGrid sparse_grid;
+    [Auto]
+    private BoxCollider spawnArea;
+    [Auto]
+
+    public bool activate_search;
+    public Collider search_collider;
 
     void Awake()
     {
-        sparse_grid = new SparseGrid<GameObject>();
+        sparse_grid = new SparseGrid();
 
         spawner.OnObjectSpawned += AddSpawnedObj_ToGrid;
     }
 
     void AddSpawnedObj_ToGrid(GameObject obj){
-        Debug.Log("Add Spawned Obj");
+        UnityEngine.Debug.Log("Add Spawned Obj");
         sparse_grid.Add(obj, obj.transform.position);
     }
+    
+        public void Update(){
+        if(activate_search)
+        {
+            ClearPrevSearchColors(); 
+            SparseGridSearch();
+        }
+    }
+    
+    List<long> search_time_measurements = new List<long>();
+    private List<GameObject> prev_search_results = new List<GameObject>();
+    
+    private List<GameObject> SparseGridSearch()
+    {
+        var sw = new Stopwatch();
+        sw.Start();
+
+        IEnumerable<GameObject> results = sparse_grid.Search(search_collider.bounds);
+
+        prev_search_results.Clear();
+        if (results != null)
+        {
+            foreach (var c_result in results)
+            {
+                c_result.GetComponent<MeshRenderer>().material.color = Color.red;
+                prev_search_results.Add(c_result);
+            }
+        }
+
+        sw.Stop();
+        if(search_time_measurements.Count >= 60){
+            search_time_measurements.RemoveAt(0);
+        }
+        search_time_measurements.Add(sw.ElapsedMilliseconds);
+        return prev_search_results;
+        // UnityEngine.Debug.Log("Linear Search: " + sw.Elapsed);
+    }
+
+    private void ClearPrevSearchColors(){
+        if (prev_search_results != null)
+        {
+            foreach (var c_result in prev_search_results)
+            {
+                c_result.GetComponent<MeshRenderer>().material.color = Color.cyan;
+            }
+        }
+
+    }
+
+
+
     
     private Transform random_obj;
     [Button]
